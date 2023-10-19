060234624981b74a782466a1457527f4a0da2cb0
ᴋᴇʏ <24655118+gh0stkey@users.noreply.github.com>
2023-10-19 22:43:29 +0800

    Version: 2.5.2 Update

--- src/main/java/burp/rule/utils/YamlTool.java
+++ src/main/java/burp/rule/utils/YamlTool.java
@@ -1,8 +1,9 @@
 package burp.rule.utils;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
-import burp.BurpExtender;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 import java.io.FileInputStream;
@@ -25,13 +26,11 @@ public class YamlTool {
 
     public static Map<String, Object> loadYaml(String filePath) {
         try {
-            InputStream inputStream = new FileInputStream(filePath);
-            Yaml yaml = newStandardYaml();
-            return yaml.load(inputStream);
-        } catch (FileNotFoundException e) {
+            InputStream inputStream = Files.newInputStream(Paths.get(filePath));
+            return newStandardYaml().load(inputStream);
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 }
-
