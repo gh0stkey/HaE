3608c3dca8c6436f215f62d133228c70c63ec20b
gh0stkey <24655118+gh0stkey@users.noreply.github.com>
2025-03-25 11:47:27 +0800

    Version: 4.1.1 Update

--- src/main/java/hae/cache/MessageCache.java
+++ src/main/java/hae/cache/MessageCache.java
@@ -4,10 +4,16 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class MessageCache {
+    private static final int MAX_SIZE = 100000;
+    private static final int EXPIRE_DURATION = 12;
+
     private static final Cache<String, Map<String, Map<String, Object>>> cache =
             Caffeine.newBuilder()
+                    .maximumSize(MAX_SIZE)
+                    .expireAfterWrite(EXPIRE_DURATION, TimeUnit.HOURS)
                     .build();
 
     public static void put(String key, Map<String, Map<String, Object>> value) {
